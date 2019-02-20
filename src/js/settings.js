3f2a2ccc401894ff7c005f38f6ad3a7284c406f3
Vladimir <mr.qrsant@gmail.com>
2019-02-20 12:50:32 +0300

    Настройки по умолчанию и доступныеперенесены в класс settings

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,41 +1,43 @@
-const vailableSettings = new Map([
-  ['theme', new Set(['dark', 'light', 'gray'])],
-  ['music', new Set(['trance', 'pop', 'rock', 'chillout', 'off'])],
-  ['difficulty', new Set(['easy', 'normal', 'hard', 'nightmare'])],
-]);
-
-const defaultSettings = new Map([
-  ['theme', 'dark'],
-  ['music', 'trance'],
-  ['difficulty', 'easy'],
-]);
-
 export default class Settings {
+  constructor() {
+    this.vailableSettings = new Map([
+      ['theme', new Set(['dark', 'light', 'gray'])],
+      ['music', new Set(['trance', 'pop', 'rock', 'chillout', 'off'])],
+      ['difficulty', new Set(['easy', 'normal', 'hard', 'nightmare'])],
+    ]);
+
+    this.defaultSettings = new Map([
+      ['theme', 'dark'],
+      ['music', 'trance'],
+      ['difficulty', 'easy'],
+    ]);
+  }
+
   set theme(newTheme) {
-    if (vailableSettings.get('theme').has(newTheme)) {
-      defaultSettings.set('theme', newTheme);
+    if (this.vailableSettings.get('theme').has(newTheme)) {
+      this.defaultSettings.set('theme', newTheme);
     } else {
       throw TypeError('Нужно выбрать тему из доступных');
     }
   }
 
   set music(newMusic) {
-    if (vailableSettings.get('music').has(newMusic)) {
-      defaultSettings.set('music', newMusic);
+    if (this.vailableSettings.get('music').has(newMusic)) {
+      this.defaultSettings.set('music', newMusic);
     } else {
       throw TypeError('Нужно выбрать музыку из доступных');
     }
   }
 
   set difficulty(newDifficulty) {
-    if (vailableSettings.get('difficulty').has(newDifficulty)) {
-      defaultSettings.set('difficulty', newDifficulty);
+    if (this.vailableSettings.get('difficulty').has(newDifficulty)) {
+      this.defaultSettings.set('difficulty', newDifficulty);
     } else {
       throw TypeError('Нужно выбрать уровень сложности из доступных');
     }
   }
 
   get settings() {
-    return defaultSettings;
+    return this.defaultSettings;
   }
 }
